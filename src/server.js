8815efe0c52d6ba36c541b132ccd5518f014aa47
andremorita <andreluizmorita@gmail.com>
2018-12-01 18:03:36 -0200

    Mongo

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
+
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    // mongodb://usuario:senha@localhost:27017/nomedatabase
+    mongoose.connect(
+      databaseConfig.uri,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true
+      }
+    )
   }
 
   middlewares () {
