85a8bcaded84802b27735413a07d6988b310d330
andremorita <andreluizmorita@gmail.com>
2018-12-02 20:32:47 -0200

    DotEnv

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'd4702f4a9326ad',
-    pass: '84997b1df0da0b'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
--- src/config/redis.js
+++ src/config/redis.js
@@ -1,4 +1,4 @@
 module.exports = {
-  host: '127.0.0.1',
+  host: process.env.REDIS_HOST,
   port: 6379
 }
