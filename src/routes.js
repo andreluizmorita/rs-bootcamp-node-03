d9897003d1f0060b01011d4d2119bb73020a804a
andremorita <andreluizmorita@gmail.com>
2018-12-02 17:31:58 -0200

    JWT token

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,12 @@ const express = require('express')
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
+routes.get('/', (req, res) => {
+  return res.send(`<h1>GoNode</h1>`)
+})
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes
