3be3892d8e175bd1ba7924e87c495679054a193f
andremorita <andreluizmorita@gmail.com>
2018-12-02 17:57:14 -0200

    Jwt

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
@@ -10,5 +12,7 @@ routes.get('/', (req, res) => {
 })
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
