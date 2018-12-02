3f69893ea03e23bda1550423c97378f6efe86093
andremorita <andreluizmorita@gmail.com>
2018-12-02 18:04:43 -0200

    Controllers Load Ad Model

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,13 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const controllers = require('./app/controllers')
 
 routes.get('/', (req, res) => {
   return res.send(`<h1>GoNode</h1>`)
 })
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
 
 routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
